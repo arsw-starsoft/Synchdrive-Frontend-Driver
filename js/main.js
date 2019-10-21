009eead1919051eb5f9311a42dab31cb76aa4010
luis572 <luispg572@gmail.com>
2019-10-21 13:06:38 -0500

    mapa

--- js/main.js
+++ js/main.js
@@ -6,25 +6,30 @@ appMapa = (function () {
         console.log('Latitud : ' + coordenadas.latitude);
         console.log('Longitud: ' + coordenadas.longitude);
         console.log('Más o menos ' + coordenadas.accuracy + ' metros.');
-        L.marker([coordenadas.latitude,coordenadas.longitude]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+        L.marker([coordenadas.latitude, coordenadas.longitude]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
     };
 
-    var cordenadasError=function (error) {
+    var cordenadasError = function (error) {
         console.warn('ERROR(' + error.code + '): ' + error.message);
     };
 
     return {
-        getCordenadas:function(name){
-            var options = {
-                enableHighAccuracy: true,
-                timeout: 6000,
-                maximumAge: 0
-              };
-            return navigator.geolocation.getCurrentPosition(cordenadaSuccess, cordenadasError, options);
+        getCordenadas: function (name) {
+            if (sessionStorage.getItem('email') == null) {
+                alert("Permiso denegado, debe logearse primero.")
+                location.href = "/loginDriver.html"
+            } else {
+                var options = {
+                    enableHighAccuracy: true,
+                    timeout: 6000,
+                    maximumAge: 0
+                };
+                return navigator.geolocation.getCurrentPosition(cordenadaSuccess, cordenadasError, options);
+            }
         }
-       
+
 
     }
 
